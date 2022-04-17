4bcc1d35eb8fcf9fcc0f6ca362e4de41e4c9679d
Jatin6256 <cs19b1013@iiitr.ac.in>
2022-04-17 16:56:27 +0530

    make deques to generate task

--- src/checkPrime.cpp
+++ src/checkPrime.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 #include "../headers/checkPrime.hpp"
 
 CheckPrime::CheckPrime()
@@ -7,14 +7,31 @@ CheckPrime::CheckPrime()
     std::cout << "CheckPrime Default Constructor" << std::endl;
 }
 
-CheckPrime::CheckPrime(int n){
+CheckPrime::CheckPrime(int n)
+{
     number = n;
     std::cout << "CheckPrime Constructor" << std::endl;
-
 }
 
-void CheckPrime::run() {
+void CheckPrime::run()
+{
 
-    std::cout << "Check Prime Run: " << number <<  std::endl;
+    std::cout << "Check Prime Run: " << number << std::endl;
+
+    if (number <= 1){
+        std::cout << "Not Prime" << std::endl;
+        return;
+    }
+
+    // Check from 2 to n-1
+    for (int i = 2; i < number; i++)
+        if (number % i == 0){
+            std::cout << number << " is Not prime" << "\n";
+            return;
+        }
+            
+
+    std::cout << number << " is prime" << "\n";
+    return;
+    
 }
-
